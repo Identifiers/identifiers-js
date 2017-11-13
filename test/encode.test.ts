0cee60c555789f18753032e5d499a66471cdd675
Matt Bishop <matthew.bishop@elasticpath.com>
2017-11-13 09:24:31 -0800

    Add tests for encode.findCodec()

--- test/encode.test.ts
+++ test/encode.test.ts
@@ -1,6 +1,50 @@
+import {expect} from "chai";
 
-describe("round-trip processing", () => {
-  it("encodes and decodes values", () => {
+import * as encode from "../src/encode";
+import {Identifier} from "../src/identifier";
+import {codecSymbol} from "../src/shared";
 
+describe("encode.findCodec() tests", () => {
+
+  it("throws an error with an identifier that is missing a codec", () => {
+    const id: Identifier<string> = {
+      type: "string",
+      value: "boo"
+    };
+    expect(() => encode.findCodec(id)).to.throw;
+  });
+
+  it("throws an error when a codec cannot encode a value", () => {
+    const codec = {
+      validateForEncoding: (value) => {
+        throw new Error();
+      }
+    };
+    const id: Identifier<number> = {
+      type: "string",
+      value: 22,
+      [codecSymbol]: codec
+    };
+
+    expect(() => encode.findCodec(id)).to.throw;
+  });
+
+  it("calls a codec's validateForEncoding method", () => {
+    let called = false;
+
+    const codec = {
+      validateForEncoding: (value) => {
+        called = true;
+        return;
+      }
+    };
+    const id = {
+      type: "number",
+      value: 22,
+      [codecSymbol]: codec
+    };
+
+    const actual = encode.findCodec(id);
+    expect(called).to.be.true;
   });
 });
