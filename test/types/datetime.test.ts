ea43757179968f485920a09ac55834059ddb3855
Matt Bishop <matthew.bishop@elasticpath.com>
2017-12-06 09:26:41 -0800

    Refactor datetime codec test to only test single methods

--- test/types/datetime.test.ts
+++ test/types/datetime.test.ts
@@ -3,19 +3,34 @@ import {expect} from "chai";
 import {datetimeCodec} from "../../src/types/datetime";
 
 describe("datetime codec", () => {
+  it("validates good identifier values", () => {
+    expect(() => datetimeCodec.validateForIdentifier(new Date())).to.not.throw();
+    expect(() => datetimeCodec.validateForIdentifier(33779563)).to.not.throw();
+  });
+
+  it("rejects bad identifier values", () => {
+    expect(() => datetimeCodec.validateForIdentifier("2017")).to.throw();
+    expect(() => datetimeCodec.validateForIdentifier(224.3)).to.throw();
+  });
+
   it("supports encoding", () => {
     const value = new Date();
-    expect(() => datetimeCodec.validateForIdentifier(value)).to.not.throw();
-    expect(() => datetimeCodec.validateForIdentifier(33779563)).to.not.throw();
     const actual = datetimeCodec.encode(value);
     expect(actual).to.equal(value.getTime());
   });
 
+  it("validates good decoded values", () => {
+    expect(() => datetimeCodec.validateForDecoding(33779563)).to.not.throw();
+  });
+
+  it("rejects bad decoded values", () => {
+    expect(() => datetimeCodec.validateForDecoding(new Date())).to.throw();
+    expect(() => datetimeCodec.validateForDecoding("2017")).to.throw();
+    expect(() => datetimeCodec.validateForDecoding(224.3)).to.throw();
+  });
+
   it("supports decoding", () => {
     const value = new Date().getTime();
-    expect(() => datetimeCodec.validateForDecoding(value)).to.not.throw();
-    expect(() => datetimeCodec.validateForDecoding(new Date())).to.throw();
-    expect(() => datetimeCodec.validateForDecoding("not a number")).to.throw();
     const actual = datetimeCodec.decode(value);
     expect(actual.getTime()).to.deep.equal(value);
   });
