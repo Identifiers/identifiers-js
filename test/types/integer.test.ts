b803853dd743513006b05aac39ec98bb1f7cc9bc
Matt Bishop <matthew.bishop@elasticpath.com>
2017-12-06 10:41:02 -0800

    Refactor integer codec test to only test single methods

--- test/types/integer.test.ts
+++ test/types/integer.test.ts
@@ -4,21 +4,19 @@ import {integerCodec} from "../../src/types/integer";
 
 
 describe("integer codec", () => {
-  describe("identifier value validation", () => {
-    it("accepts 32-bit integer signed values", () => {
-      expect(() => integerCodec.validateForIdentifier(-100)).to.not.throw();
-      expect(() => integerCodec.validateForIdentifier(0)).to.not.throw();
-      expect(() => integerCodec.validateForIdentifier(10000)).to.not.throw();
-      expect(() => integerCodec.validateForIdentifier((2 ** 31) - 1)).to.not.throw();
-      expect(() => integerCodec.validateForIdentifier(-(2 ** 31))).to.not.throw();
-    });
+  it("validates good identifier values", () => {
+    expect(() => integerCodec.validateForIdentifier(-100)).to.not.throw();
+    expect(() => integerCodec.validateForIdentifier(0)).to.not.throw();
+    expect(() => integerCodec.validateForIdentifier(10000)).to.not.throw();
+    expect(() => integerCodec.validateForIdentifier((2 ** 31) - 1)).to.not.throw();
+    expect(() => integerCodec.validateForIdentifier(-(2 ** 31))).to.not.throw();
+  });
 
-    it("does not accept longs, other non-number values", () => {
-      expect(() => integerCodec.validateForIdentifier(20.223)).to.throw();
-      expect(() => integerCodec.validateForIdentifier(787869585484)).to.throw();
-      expect(() => integerCodec.validateForIdentifier((2 ** 31))).to.throw();
-      expect(() => integerCodec.validateForIdentifier(-(2 ** 31) - 1)).to.throw();
-    });
+  it("rejects bad identifier values", () => {
+    expect(() => integerCodec.validateForIdentifier(20.223)).to.throw();
+    expect(() => integerCodec.validateForIdentifier(787869585484)).to.throw();
+    expect(() => integerCodec.validateForIdentifier((2 ** 31))).to.throw();
+    expect(() => integerCodec.validateForIdentifier(-(2 ** 31) - 1)).to.throw();
   });
 
   it("supports encoding", () => {
