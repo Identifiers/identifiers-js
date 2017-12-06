1e73a50fff477aa549b7450e3db2d7cd6223c330
Matt Bishop <matthew.bishop@elasticpath.com>
2017-12-06 09:19:03 -0800

    Refactor boolean codec test to only test single methods

--- test/types/boolean.test.ts
+++ test/types/boolean.test.ts
@@ -4,18 +4,35 @@ import {booleanCodec} from "../../src/types/boolean";
 
 
 describe("boolean codec", () => {
+  it("validates good values for identifier", () => {
+    expect(() => booleanCodec.validateForIdentifier(true)).to.not.throw();
+    expect(() => booleanCodec.validateForIdentifier(false)).to.not.throw();
+  });
+
+  it("rejects bad values for identifier", () => {
+    expect(() => booleanCodec.validateForIdentifier(1)).to.throw();
+    expect(() => booleanCodec.validateForIdentifier(null)).to.throw();
+  });
+
   it("supports encoding", () => {
     const value = false;
-    expect(() => booleanCodec.validateForIdentifier(value)).to.not.throw();
-    expect(() => booleanCodec.validateForIdentifier(1)).to.throw();
     const actual = booleanCodec.encode(value);
     expect(actual).to.equal(value);
   });
 
+  it("validates good decode values", () => {
+    expect(() => booleanCodec.validateForDecoding(true)).to.not.throw();
+    expect(() => booleanCodec.validateForDecoding(false)).to.not.throw();
+  });
+
+  it("invalidates bad decode values", () => {
+    expect(() => booleanCodec.validateForDecoding(0)).to.throw();
+    expect(() => booleanCodec.validateForDecoding("false")).to.throw();
+    expect(() => booleanCodec.validateForDecoding(null)).to.throw();
+  });
+
   it("supports decoding", () => {
     const value = true;
-    expect(() => booleanCodec.validateForDecoding(value)).to.not.throw();
-    expect(() => booleanCodec.validateForDecoding(1)).to.throw();
     const actual = booleanCodec.decode(value);
     expect(actual).to.equal(value);
   });
