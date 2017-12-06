6c04de24e77b60daea368c1481ae2c75c207796c
Matt Bishop <matthew.bishop@elasticpath.com>
2017-12-06 10:51:13 -0800

    Refactor string codec test to only test single methods

--- test/types/string.test.ts
+++ test/types/string.test.ts
@@ -4,19 +4,35 @@ import {stringCodec} from "../../src/types/string";
 
 
 describe("string codec", () => {
-  it("supports encoding", () => {
-    const value = "ballast";
-    expect(() => stringCodec.validateForIdentifier(value)).to.not.throw();
+  it("validates good identifier values", () => {
+    expect(() => stringCodec.validateForIdentifier("maximus")).to.not.throw();
+    expect(() => stringCodec.validateForIdentifier("")).to.not.throw();
+  });
+
+  it("rejects bad identifier values", () => {
+    expect(() => stringCodec.validateForIdentifier(false)).to.throw();
     expect(() => stringCodec.validateForIdentifier(1)).to.throw();
+  });
+
+  it("supports encoding", () => {
+    const value = "ships";
     const actual = stringCodec.encode(value);
     expect(actual).to.equal(value);
-  })
+  });
 
-  it("supports decoding", () => {
-    const value = "ballast";
-    expect(() => stringCodec.validateForDecoding(value)).to.not.throw();
+  it("validates good decoded values", () => {
+    expect(() => stringCodec.validateForDecoding("maximus")).to.not.throw();
+    expect(() => stringCodec.validateForDecoding("")).to.not.throw();
+  });
+
+  it("rejects bad decoded values", () => {
+    expect(() => stringCodec.validateForDecoding(false)).to.throw();
     expect(() => stringCodec.validateForDecoding(1)).to.throw();
-    const actual = stringCodec.decode(value);
-    expect(actual).to.equal(value);
+
+    it("supports decoding", () => {
+      const value = "ballast";
+      const actual = stringCodec.decode(value);
+      expect(actual).to.equal(value);
+    });
   });
 });
