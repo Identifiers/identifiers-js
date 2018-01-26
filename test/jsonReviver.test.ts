be3e4d68c4da15a13c73826b7f9931be139b6714
Matt Bishop <matthew.bishop@elasticpath.com>
2018-01-25 17:23:07 -0800

    Add test for badly-encoded strings in JSON.

--- test/jsonReviver.test.ts
+++ test/jsonReviver.test.ts
@@ -7,14 +7,16 @@ describe("jsonReviver", () => {
   it("turns valid encoded identifiers in JSON into identifiers", () => {
     const id1 = factory.integer(1);
     const id2 = factory.string("2");
-    const obj = { a: id1, b: { c: id2 } };
+    const obj = { a: id1, b: { cc: id2}, c: false, d: "_fake-encoded" };
 
     const json = JSON.stringify(obj);
     const parsed = JSON.parse(json, JSON_reviver);
 
     expect(parsed.a.type).to.equal(id1.type);
     expect(parsed.a.value).to.equal(id1.value);
-    expect(parsed.b.c.type).to.equal(id2.type);
-    expect(parsed.b.c.value).to.equal(id2.value);
+    expect(parsed.b.cc.type).to.equal(id2.type);
+    expect(parsed.b.cc.value).to.equal(id2.value);
+    expect(parsed.c).to.equal(obj.c);
+    expect(parsed.d).to.equal(obj.d);
   });
 });
