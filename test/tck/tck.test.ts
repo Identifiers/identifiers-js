607355b9c645b259d16338c584f4608c96c02ec0
Matt Bishop <matthew.bishop@elasticpath.com>
2018-04-16 19:43:10 -0700

    Convert TCK test to TypeScript so it's part of unit tests.

---
+++ test/tck/tck.test.ts
@@ -0,0 +1,88 @@
+import * as chai from "chai";
+const expect = chai.expect;
+
+import * as IDs from "../../src";
+
+const codecSymbol = Symbol.for("id-codec");
+
+
+describe("TCK tests", () => {
+  describe("primitives", () => {
+    it("supports string", () => {
+      const tck = require("identifiers-spec/tck/files/primitives/string.json");
+      testTck(tck);
+    });
+
+    it("supports boolean", () => {
+      const tck = require("identifiers-spec/tck/files/primitives/boolean.json");
+      testTck(tck);
+    });
+
+    it("supports integer", () => {
+      const tck = require("identifiers-spec/tck/files/primitives/integer.json");
+      testTck(tck);
+    });
+
+    it("supports float", () => {
+      const tck = require("identifiers-spec/tck/files/primitives/float.json");
+      testTck(tck);
+    });
+
+    it("supports long", () => {
+      const tck = require("identifiers-spec/tck/files/primitives/long.json");
+      testTck(tck);
+    });
+
+    it("supports bytes", () => {
+      const tck = require("identifiers-spec/tck/files/primitives/bytes.json");
+      testTck(tck);
+    });
+  });
+
+  describe("semantic", () => {
+    it("supports uuid", () => {
+      const tck = require("identifiers-spec/tck/files/semantic/uuid.json");
+      testTck(tck);
+    });
+
+    it("supports datetime", () => {
+      const tck = require("identifiers-spec/tck/files/semantic/datetime.json");
+      testTck(tck);
+    });
+
+    it("supports geo", () => {
+      const tck = require("identifiers-spec/tck/files/semantic/geo.json");
+      testTck(tck);
+    });
+  });
+});
+
+
+interface TCK {
+  typeCode: number;
+  type: string;
+  value: any;
+  base128: string;
+  base32: string;
+}
+
+function testTck(tck: TCK[]): void {
+  tck.forEach(test => {
+    roundTripTest(test, test.base128);
+    roundTripTest(test, test.base32, true);
+  });
+}
+
+function roundTripTest(test: TCK, encoded: string, isBase32?: boolean): void {
+  const id = IDs.decodeFromString(encoded);
+  expect(id.type).to.equal(test.type);
+  // JSON.stringify strips out the functions so we can just compare values.
+  expect(JSON.stringify(id.value)).to.equal(JSON.stringify(test.value));
+
+  const toString = isBase32 ? id.toBase32String() : id.toString();
+  expect(toString).to.equal(encoded);
+
+  // @ts-ignore: codec not part of identifier interface
+  const codec = id[codecSymbol];
+  expect(codec.typeCode).to.equal(test.typeCode);
+}
