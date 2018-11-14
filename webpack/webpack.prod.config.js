03b247a9d27ce96ba6ef6a43a075db291836cc1c
Matt Bishop <matt@thebishops.org>
2018-11-14 07:32:58 -0800

    webpack 'library' setting makes it easier to use identifiers on a web page.

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -5,6 +5,7 @@ module.exports = {
   mode: 'production',
   entry: path.resolve(__dirname, '../src/index.ts'),
   output: {
+    library: "identifiers",
     filename: 'identifiers.bundle.js',
     path: path.resolve(__dirname, '../dist')
   },
